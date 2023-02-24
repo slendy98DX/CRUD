75274c45760b648d3de49301f54d1af911703064
slendy98DX <marco.setaro93@gmail.com>
2023-02-24 17:22:31 +0100

    fix

--- CRUD/src/main/java/co/develhope/CRUD/repositories/CarRepository.java
+++ CRUD/src/main/java/co/develhope/CRUD/repositories/CarRepository.java
@@ -2,6 +2,8 @@ package co.develhope.CRUD.repositories;
 
 import co.develhope.CRUD.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car,String> {
 }
